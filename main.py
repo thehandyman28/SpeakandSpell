629d22f60d8145849a4321d5dac7cb8c145ecdb4
thehandyman28 <51925740+thehandyman28@users.noreply.github.com>
2024-05-20 21:42:30 -0400

    v3

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<a href='/foo'>Hello, World!</a>"
+    return "<a href='/login'>spelling</a>"
 @app.route("/foo")
 def foo():
     return "<h1>foobar2</h1>"
 @app.route("/login")
 def login():
-    with open("index.html") as f:
-        lines=f.readlines()
-        return "".join(lines)
+    return render_template("index.html")
+    # with open("index.html") as f:
+    #     lines=f.readlines()
+    #     return "".join(lines)
 if __name__=="__main__":
     app.run(debug=True)
